fc3f1e6ad7d3ce6f6360f8140311061638685ff1
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-10-01 00:45:50 -0300

    add fusca

---
+++ source/repos/APIBoletim/Interfaces/IAluno.cs
@@ -0,0 +1,17 @@
+﻿using APIBoletim.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Interfaces {
+    interface IAluno {
+
+        //Lerning DDD Interface
+        Aluno Register(Aluno a);
+        List<Aluno> ReadAll();
+        Aluno SearchbyId(int id);
+        Aluno Chage(Aluno a);
+        Aluno Delete(Aluno a);
+    }
+}
---
+++ source/repos/APIBoletim/Repositories/AlunoRepository.cs
@@ -0,0 +1,30 @@
+﻿using APIBoletim.Domains;
+using APIBoletim.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIBoletim.Repositories {
+    public class AlunoRepository : IAluno {
+        public Aluno Chage(Aluno a) {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Delete(Aluno a) {
+            throw new NotImplementedException();
+        }
+
+        public List<Aluno> ReadAll() {
+            throw new NotImplementedException();
+        }
+
+        public Aluno Register(Aluno a) {
+            throw new NotImplementedException();
+        }
+
+        public Aluno SearchbyId(int id) {
+            throw new NotImplementedException();
+        }
+    }
+}
